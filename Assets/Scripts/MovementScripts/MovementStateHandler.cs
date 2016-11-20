2a0c721f1ebb4d98c351be20aa325a06fd2fd170
JdRo623 <JdRodriguez623@outlook.com>
2016-11-20 18:50:17 -0500

    firstt

---
+++ Assets/Scripts/MovementScripts/MovementStateHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementStateHandler : MonoBehaviour
+{
+    public static GameObject mob;
+    public GameObject player;
+    private StateMoveDown moveDown;
+    private StateMoveUp moveUp;
+    private StateMoveRigth moveRigth;
+    private StateMoveLeft moveLeft;
+    private RotationHandler playerMovement;
+    private StateMoveForward moveFoward;
+    public MovementState currentState;
+    // Use this for initialization
+    void Awake() {
+        mob = player;
+        moveFoward = GetComponent<StateMoveForward>();
+        moveDown = GetComponent<StateMoveDown>();
+        moveUp = GetComponent<StateMoveUp>();
+        moveLeft = GetComponent<StateMoveLeft>();
+        moveRigth = GetComponent<StateMoveRigth>();
+        playerMovement = GetComponent<RotationHandler>();
+        
+        initConfig();
+        
+    }
+    void Start()
+    {
+ 
+    }
+    public void initConfig()
+    {
+        moveFoward.enabled = true;
+        moveDown.enabled = false;
+        moveUp.enabled = false;
+        moveLeft.enabled = false;
+        moveRigth.enabled = false;
+        playerMovement.enabled = false;
+        currentState = playerMovement;
+        currentState.enabled = true;
+
+    }
+    void setNewState(MovementState newState) {
+        currentState.enabled = false;
+        currentState = newState;
+        currentState.enabled = true;
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "RigthBoundary") {
+            Debug.Log("dasfdsf");
+            setNewState(moveLeft);
+        }
+        else if (other.tag == "LeftBoundary") {
+            Debug.Log("dasfdsf");
+            setNewState(moveRigth);
+        }
+        else if(other.tag == "UpBoundary")
+        {
+            Debug.Log("dasfdsf");
+            setNewState(moveDown);
+        }
+        else if(other.tag == "DownBoundary")
+        {
+            Debug.Log("dasfdsf");
+            setNewState(moveUp);
+        }
+    }
+
+}
---
+++ Assets/Scripts/MovementScripts/RotationHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationHandler: MovementState {
+    public Vector3 movementVector;
+    public float speedSides;
+    public Quaternion quack;
+	// Use this for initialization
+	void OnEnable () {
+
+       movementVector=new Vector3(0,0,0);
+}
+
+// Update is called once per frame
+   void Update () {
+        SetRotation();
+        MoveSides();
+    }
+    void SetRotation() {
+        //this.transform.rotation = Camera.main.transform.rotation;
+       // this.transform.rotation = Camera.main.transform.rotation;
+    quack = new Quaternion(Camera.main.transform.rotation.x
+    , this.transform.rotation.x
+    , this.transform.rotation.z
+    , this.transform.rotation.w);
+
+    //
+    }
+    void MoveSides() {
+
+        // movementVector.y = Camera.main.transform.rotation.x * speedSides *1 * Time.deltaTime;
+        movementVector.x = Camera.main.transform.localRotation.z * speedSides*-1*Time.deltaTime;
+    }
+    void Move2CenterFromSides(int side) {
+
+    }
+    void OnTriggerEnter(Collider other) {
+    }
+}
